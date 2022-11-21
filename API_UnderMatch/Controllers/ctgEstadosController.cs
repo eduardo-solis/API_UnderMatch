96c6dab45561a3f110832fa8ed8a0ddf16789e5b
josueduran08z <josueduran08z@gmail.com>
2022-11-21 09:37:09 -0600

    Se crearon los controladores de estados y municipios

---
+++ API_UnderMatch/Controllers/ctgEstadosController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API_UnderMatch.Models;
+
+namespace API_UnderMatch.Controllers
+{
+    public class ctgEstadosController : ApiController
+    {
+        private BDUnderMatchEntities1 db = new BDUnderMatchEntities1();
+
+        // GET: api/ctgEstados
+        public IQueryable<ctgEstados> GetctgEstados()
+        {
+            return db.ctgEstados;
+        }
+
+        // GET: api/ctgEstados/5
+        [ResponseType(typeof(ctgEstados))]
+        public IHttpActionResult GetctgEstados(int id)
+        {
+            ctgEstados ctgEstados = db.ctgEstados.Find(id);
+            if (ctgEstados == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ctgEstados);
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ctgEstadosExists(int id)
+        {
+            return db.ctgEstados.Count(e => e.IdEstado == id) > 0;
+        }
+    }
+}
---
+++ API_UnderMatch/Controllers/ctgMunicipiosController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API_UnderMatch.Models;
+
+namespace API_UnderMatch.Controllers
+{
+    public class ctgMunicipiosController : ApiController
+    {
+        private BDUnderMatchEntities1 db = new BDUnderMatchEntities1();
+
+        // GET: api/ctgMunicipios
+        public IQueryable<ctgMunicipios> GetctgMunicipios(int idEstado)
+        {
+            List<Estados_Municipios> estadosMunicipios = db.Estados_Municipios.Where(e => e.IdEstado == idEstado).ToList();
+            List<int> idsMunicipios = new List<int>();
+
+            for (int i = 0; i < estadosMunicipios.Count; i++)
+            {
+                idsMunicipios.Add(estadosMunicipios[i].IdMunicipio);
+            }
+
+            List<ctgMunicipios> municipios = db.ctgMunicipios.Where(m => idsMunicipios.Contains(m.IdMunicipio)).ToList();
+
+            return municipios.AsQueryable();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ctgMunicipiosExists(int id)
+        {
+            return db.ctgMunicipios.Count(e => e.IdMunicipio == id) > 0;
+        }
+    }
+}
