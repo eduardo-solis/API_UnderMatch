b37b51d316c25cfa56c614a5815386e35e6ff387
josueduran08z <josueduran08z@gmail.com>
2022-11-28 12:34:09 -0600

    Se modifico el modelo

--- API_UnderMatch/Models/ctgTipoProveedores.cs
+++ API_UnderMatch/Models/ctgTipoProveedores.cs
@@ -9,6 +9,7 @@
 
 namespace API_UnderMatch.Models
 {
+    using Newtonsoft.Json;
     using System;
     using System.Collections.Generic;
     
@@ -25,6 +26,7 @@ namespace API_UnderMatch.Models
         public Nullable<int> Estatus { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        [JsonIgnore]
         public virtual ICollection<tblProveedores> tblProveedores { get; set; }
     }
 }
